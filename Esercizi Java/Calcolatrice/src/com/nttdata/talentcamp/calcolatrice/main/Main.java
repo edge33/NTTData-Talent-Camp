127a611a2523bfb1f2e2ab3c5890457a695ae649
Francesco <francesco.maida@nttdata.com>
2019-11-05 16:33:15 +0100

    aggiunti metodi di test per calcolatrice

--- Esercizi Java/Calcolatrice/src/com/nttdata/talentcamp/calcolatrice/main/Main.java
+++ Esercizi Java/Calcolatrice/src/com/nttdata/talentcamp/calcolatrice/main/Main.java
@@ -3,7 +3,15 @@ package com.nttdata.talentcamp.calcolatrice.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		int a = 10;
+		int b = 8;
+		
+		int somma = Calcolatrice.somma(a,b);
+		System.out.println("La somma è:" + somma);
+		
+		int differenza = Calcolatrice.differenza(a, b);
+		System.out.println("La differenza è: " + differenza);
 	}
 
 }
